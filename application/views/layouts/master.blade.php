f0dbff7c2d846ce3c724ce79a93ccaf7036ed773
Bobby Hazel <rhazel178@g.rwu.edu>
2013-02-10 21:56:51 -0500

    changed title

--- application/views/layouts/master.blade.php
+++ application/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>BookConnect</title>
+	<title>TextConnect</title>
 	<meta name="viewport" content="width=device-width">
 	{{ Asset::container('bootstrapper')->styles();}}  
 	{{ HTML::style('css/font-awesome.min.css') }}
